d4f4123cfed77b0cb60b3d37091b5e57c62d30cf
rohildshah <shah5963@gmail.com>
2021-12-20 13:18:01 -0800

    decent approximation using average

--- generate-e.py
+++ generate-e.py
@@ -1,9 +1,12 @@
 import random
 
-sum = 0     # running sum
-count = 0   # number of numbers summed so far
-while (sum < 1.0):
-    count += 1
-    sum += random.random()
+countSum = 0   # sum of numbers necessary to get past 1.0
+for i in range (1, 1001):
+    sum = 0         # running sum
+    
+    while (sum < 1.0):
+        countSum += 1
+        sum += random.random()
 
-print(count)
+    if (i % 100 == 0):
+        print(countSum/i)
